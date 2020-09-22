63cea214e414a43e21758c1bcfd6235074ea0dcf
notend <xyqinsong@163.com>
2020-09-23 00:05:19 +0800

    fix: fix

--- javascript/achieve.js
+++ javascript/achieve.js
@@ -63,29 +63,53 @@ Function.prototype.myBind = function (context, ...args) {
 	}
 }
 
-function polyfillBind (fn, ctx){
-	function boundFn(a){
-		var l = arguments.length;
-		return l
-			? l > 1
-				? fn.apply(ctx, arguments)
-				: fn.call(ctx, a)
-			: fn.call(ctx)
+function bind3(asThis) {
+	var slice = Array.prototype.slice;
+	var args1 = slice.call(arguments, 1);
+	var fn = this;
+	if (typeof fn !== "function") {
+		throw new Error("Must accept function");
 	}
+	function resultFn() {
+		var args2 = slice.call(arguments, 0);
+		return fn.call(
+			resultFn.prototype.isPrototypeOf(this) ? this : asThis, // new 的情况下 this 改绑成 new 出来的对象实例
+			args1.concat(args2)
+		);
+	}
+	resultFn.prototype = fn.prototype;
+	return resultFn;
 }
 
 // obj.sayName.myBind(b, 12, 31)()
 
+function Create() {
+	// 创建一个空对象
+	const object = {}
+	// 获取构造函数
+	const Constructor = [].shift.call(arguments)
+	// 链接到原型
+	object.__proto__ = Constructor.prototype
+	// 绑定this 执行构造函数
+	const result = Constructor.apply(object, arguments)
+	// 确保new出来是个对象
+	return typeof result === 'object' ? result : object
+}
+// test
 function Point(x, y) {
 	this.x = x
 	this.y = y
 }
+const point = Create(Point, 1, 2)
+
 
 Point.prototype.toString = function () {
 	console.log(this.x + ',' + this.y)
 }
 var emptyObj = {}
-var YAxisPoint = Point.myBind(emptyObj, 0/*x*/)
+// var YAxisPoint = Point.myBind(emptyObj, 0/*x*/)
+Function.prototype.bind3 = bind3
+var YAxisPoint = Point.bind3(emptyObj, 0/*x*/)
 
 // 本页下方的 polyfill 不支持运行这行代码，
 // 但使用原生的 bind 方法运行是没问题的：
@@ -122,3 +146,35 @@ function curry(func) {
 	}
 
 }
+
+
+class EventHub {
+	cache = {};
+	on(eventName, fn) {
+		this.cache[eventName] = this.cache[eventName] || [];
+		return this.cache[eventName].push(fn) -1; // push会返回index, 暂时用key作为键值
+	}
+	emit(eventName) {
+		this.cache[eventName].forEach((fn) => fn());
+	}
+	off(eventName, fn) {
+		const index = this.cache[eventName].findIndex(item => item === fn)
+		if (index === -1) return;
+		this.cache[eventName].splice(index, 1);
+	}
+	offIndex(eventName, index) {
+		this.cache[eventName].splice(index, 1);
+	}
+}
+
+const event = new EventHub()
+function test11 () {
+	console.log(111)
+}
+const index = event.on('test1', test11)
+console.log(event)
+event.offIndex('test1', index)
+console.log(event)
+// setTimeout(() => {
+// 	event.emit('test1')
+// }, 3000)
--- javascript/deepClone.js
+++ javascript/deepClone.js
@@ -1,21 +1,18 @@
-function isObject(target){
+function isObject(target) {
 	const type = typeof target
 	return target !== null && (type === 'object' || type === 'function')
 }
 
-console.log(isObject(Symbol('ts')))
 
-
-function deepClone(target, hashMap = new WeakMap()){
-	if (target === null) return target // 如果是null不进行拷贝操作
-	// 可能是对象或者普通的值  如果是函数的话是不需要深拷贝
-	if (typeof target !== 'object') return target
+function deepClone(target, hashMap = new WeakMap()) {
+	if (target === null) return target // typeof null === 'object'
+	if (typeof target !== 'object') return target // undefined, string, number, function, symbol
 	// 是对象的话就要进行深拷贝
-
+	// 处理循环引用
 	if (hashMap.get(target)) {
 		return hashMap.get(target)
 	}
-
+	
 	let cloneTarget = new target.constructor()
 	// 找到的是所属类原型上的constructor,而原型上的 constructor指向的是当前类本身
 	// 防止循环引用
@@ -50,6 +47,9 @@ function deepClone(target, hashMap = new WeakMap()){
 const map = new Map()
 map.set('key', 'value')
 map.set('sysuke', '1sdfsad')
+map.set('obj', {
+	test: 'map-obj'
+})
 const obj = {
 	test: 'hello'
 }
@@ -58,12 +58,16 @@ obj.child = obj
 const set = new Set()
 set.add('tom')
 set.add('jack')
+set.add({
+	test: 'set-obj'
+})
 
 const target = {
 	num: 1,
 	notNum: NaN,
 	unD: undefined,
 	empty: null,
+	bool: false,
 	map,
 	set,
 	array: [2, 4, 8],
@@ -78,24 +82,24 @@ const target = {
 	func1: () => {
 		console.log('test fun')
 	},
-	func2: function(a, b){
+	func2: function (a, b) {
 		return a + b
 	}
 }
 
 
-console.log(target)
-const result = deepClone(target)
-target.bool = 'boolean11'
-console.log(result)
+// console.log(target)
+// const result = deepClone(target)
+// target.bool = 'boolean11'
+// console.log(result)
 
 
-function createData(deep, breadth){
+function createData(deep, breadth) {
 	const data = {}
 	let temp = data
-	for (let i=0; i< deep; i++) {
+	for (let i = 0; i < deep; i++) {
 		temp = temp['tt'] = {}
-		for (let j=0; j <breadth; j++) {
+		for (let j = 0; j < breadth; j++) {
 			temp[j] = j
 		}
 	}
@@ -103,78 +107,139 @@ function createData(deep, breadth){
 }
 
 
-
-let count = 1
-function deepClone2(source, hashMap = new WeakMap()){
-
+function deepClone2(source, hashMap = new WeakMap()) {
+	
 	let cloneData = new source.constructor()
 	// 循环数组
 	const loopList = [
 		{
 			origin: source,
-			key: undefined,
+			parent: null,
+			parentType: null,
+			dataKey: null,
 			data: cloneData,
 		}
 	]
-
+	
 	while (loopList.length) {
-		count++
 		// 深度优先遍历
 		const node = loopList.pop()
 		const origin = node.origin
-		// const key = node.key
-		const data = node.data
+		const parent = node.parent
+		const parentType = node.parentType
+		let data = node.data
+		let dataKey = node.dataKey
 		const stringType = Object.prototype.toString.call(origin)
-
+		
+		if (hashMap.get(origin)) {
+			data = hashMap.get(origin)
+			if (parent && dataKey) {
+				parentType === '[object Set]' ? parent.add(data) : parent[dataKey] = data
+			}
+			continue
+		} else hashMap.set(origin, data)
+		
 		switch (stringType) {
-			case '[object Set]':
-				origin.forEach(v => {
-
-				})
-				break
-			case '[object Map]':
-				origin.forEach((v, k) => data.set(k, deepClone(v, hashMap)))
-				break
-			case '[object Array]':
-				origin.forEach((v, index) => data[index] = deepClone(v, hashMap))
-				break
 			case '[object Object]':
 				const keys = Object.keys(origin)
 				keys.forEach(key => {
 					if (needDeepClone(origin[key])) {
-
-						if (hashMap.get(origin[key])) {
-							data[key] = origin[key]
-						} else {
-							loopList.push({
-								origin: origin[key],
-								data: new origin[key].constructor()
-							})
+						loopList.push({
+							origin: origin[key],
+							parent: data,
+							parentType: stringType,
+							dataKey: key,
+							data: new origin[key].constructor()
+						})
+					} else {
+						data[key] = copyBaseValue(origin[key])
+						if (parent && dataKey) {
+							parentType === '[object Set]' ? parent.add(data) : parent[dataKey] = data
 						}
-
-					} data[key] = copyBaseValue(origin[key])
+					}
+				})
+				break
+			
+			case '[object Array]':
+				origin.forEach((v, index) => {
+					if (needDeepClone(v)) {
+						loopList.push({
+							origin: v,
+							parent: data,
+							dataKey: index,
+							data: new v.constructor()
+						})
+					} else {
+						data[index] = copyBaseValue(v)
+						if (parent && dataKey) {
+							parentType === '[object Set]' ? parent.add(data) : parent[dataKey] = data
+						}
+					}
+				})
+				break
+			
+			case '[object Set]':
+				origin.forEach(v => {
+					if (needDeepClone(v)) {
+						loopList.push({
+							origin: v,
+							parent: data,
+							dataKey: v,
+							data: new v.constructor()
+						})
+					} else {
+						data.add(copyBaseValue(v))
+						if (parent && dataKey) {
+							parentType === '[object Set]' ? parent.add(data) : parent[dataKey] = data
+						}
+					}
+				})
+				break
+			case '[object Map]':
+				origin.forEach((v, k) => {
+					if (needDeepClone(v)) {
+						loopList.push({
+							origin: v,
+							parent: data,
+							dataKey: k,
+							data: new v.constructor()
+						})
+					} else {
+						data.set(k, copyBaseValue(v))
+						if (parent && dataKey) {
+							parentType === '[object Set]' ? parent.add(data) : parent[dataKey] = data
+						}
+					}
 				})
 				break
 		}
-
 	}
-
-
+	
+	
 	return cloneData
 }
 
 
-
-function needDeepClone(target){
+function needDeepClone(target) {
 	const stringType = Object.prototype.toString.call(target)
 	return ['[object Set]', '[object Map]', '[object Array]', '[object Object]'].includes(stringType)
 }
+
 function copyBaseValue(target) {
-	if (target === null) return target // 如果是null不进行拷贝操作
-	// 可能是对象或者普通的值  如果是函数的话是不需要深拷贝
+	if (target === null) return target // typeof null === 'object'
 	if (typeof target !== 'object') return target
 	return new target.constructor(target)
 }
+const obj2 = createData(112122, 2)
+// console.log(obj2)
+// const copyObj = deepClone2(obj2)
 
-console.log(deepClone2(createData(10000, 2)))
-console.log(count)
+// obj2['tt'] = 'sdo'
+// console.log(obj2)
+// console.log(copyObj)
+target.obj2 = obj2
+console.log(target)
+const result = deepClone2(target)
+target.bool = 'boolean11'
+obj2.test = 1121
+console.log(result)
